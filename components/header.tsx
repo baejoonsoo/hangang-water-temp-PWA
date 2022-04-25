7852bcd267c3d11836d0ebe424282e6ad44f3e2a
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-04-25 19:25:04 +0900

    햄버거 메뉴

--- components/header.tsx
+++ components/header.tsx
@@ -1,21 +1,31 @@
 import * as S from "../styles/header";
 import { Link } from "react-scroll";
+import { useRef, useState } from "react";
 
 interface props {
   station: string;
 }
 
 const Header = ({ station }: props) => {
+  const [menuOpen, setMenuOpen] = useState<boolean>(false);
+
+  const menuClick = () => {
+    setMenuOpen((pre) => !pre);
+  };
+
   return (
     <S.Header>
-      <div>
-        <S.Logo src="/img/Logo.svg" />
-        <S.Title>
-          <p className="title">지금 한강은</p>
-          <p className="subTitle">{station && `${station} 한강물 온도`}</p>
-        </S.Title>
+      <div className="logoDiv">
+        <div className="logo">
+          <S.Logo src="/img/Logo.svg" />
+          <S.Title>
+            <p className="title">지금 한강은</p>
+            <p className="subTitle">{station && `${station} 한강물 온도`}</p>
+          </S.Title>
+        </div>
+        <S.MobileMenu onClick={menuClick} />
       </div>
-      <S.Nav>
+      <S.Nav isOpen={menuOpen}>
         <Link to="temp" spy={true} smooth={true}>
           <p>temp</p>
         </Link>
--- styles/header.ts
+++ styles/header.ts
@@ -8,18 +8,34 @@ export const Header = styled.header`
   align-items: center;
   justify-content: space-between;
 
-  background-color: rgba(40, 40, 40, 0.5);
+  background-color: rgba(40, 40, 40, 0.6);
 
   position: fixed;
   top: 0;
   left: 0;
   z-index: 1;
 
-  div {
+  .logoDiv {
     display: flex;
     align-items: center;
     height: 80px;
     width: fit-content;
+  }
+
+  & > .logoDiv > .logo {
+    height: 80px;
+
+    display: flex;
+    align-items: center;
+  }
+
+  @media screen and (max-width: 600px) {
+    flex-direction: column;
+
+    & .logoDiv {
+      width: 100vw;
+      justify-content: space-between;
+    }
   }
 `;
 
@@ -66,5 +82,30 @@ export const Nav = styled.nav`
       cursor: pointer;
       color: #4263eb;
     }
+  }
+
+  @media screen and (max-width: 600px) {
+    flex-direction: column;
+    height: fit-content;
+    width: 100vw;
+    margin: 0;
+    background-color: rgba(80, 80, 80, 0.6);
+    transform: scaleY(${({ isOpen }: { isOpen: boolean }) => (isOpen ? 1 : 0)});
+  }
+`;
+
+export const MobileMenu = styled.div`
+  height: 40px;
+  width: 40px;
+  padding-right: 80px;
+
+  background-image: url("/img/hamburger.svg");
+  background-position: center;
+  background-size: contain;
+  background-repeat: no-repeat;
+  display: none;
+
+  @media (max-width: 600px) {
+    display: block;
   }
 `;
