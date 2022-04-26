7bddcac3923061fc5ac4fcccbf2296a80d4cbb18
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-04-26 11:49:15 +0900

    title 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,17 @@ import "../styles/globals.css";
 import "../styles/font.css";
 
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Head>
+        <title>지금 한강은</title>
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp;
